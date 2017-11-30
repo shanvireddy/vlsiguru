1e2af949b6188f783a5b45a2c3304ceb292e84f0
vanitha <31364598+shanvireddy@users.noreply.github.com>
2017-11-30 23:11:21 +0530

    Add files via upload

--- Embedded_systems/Assignment/27-Nov-2017/ReadFileToLinkedList.c
+++ Embedded_systems/Assignment/27-Nov-2017/ReadFileToLinkedList.c
@@ -1,9 +1,27 @@
+/* Program is to read a paragraph from the file into the linked list and search for a word and replace it with another word */
+
 #include<stdio.h>
 #include<malloc.h>
 #include<malloc.h>
 #include<stdlib.h>
+#include<string.h>
+
 void createlist(char *buff);
 void DisplayList();
+void SearchListReplace();
+
+int DisplayMenu()
+{
+	int choice;
+	printf("enter your choice:\n\n");
+	printf("1.Dislay the list\n");
+	printf("2.searchlist and Replace\n");
+	printf("3.Exit\n\n");
+	printf("......................\n");
+	scanf("%d",&choice);
+	printf("......................\n");
+	return choice;
+}
 typedef struct node 
     {
        char data[30] ;
@@ -19,8 +37,11 @@ typedef struct node
  	FILE *ptr;       // file pointer
  	char filename[10];
  	int retval;
- 	char str[1024];
- 	char *token;     
+ 	char str[1024];  
+ 	char *token; 
+	 int flag;
+	 int choice; 
+	  flag=1;   
  	
  	printf("enter the filename\n");
  	scanf("%s",filename);
@@ -41,13 +62,35 @@ typedef struct node
 			token= strtok(NULL, " ,\n.");
 		}
 	}
-	DisplayList();
 	fclose(ptr);
+	while (flag)
+        {
+             choice = DisplayMenu() ;
+             switch(choice)
+             {  
+                case 1:
+                {
+                    DisplayList() ;
+                    break ;
+                }
+                case 2:
+                {
+                    SearchListReplace();
+                    break ;
+                }
+                case 3:
+                {
+                    flag = 0 ;
+                    break ;
+                }
+                default:
+                {
+                   break ;
+                }
+             }
+       }
  }
- void searchWord()
- {
- 	
- }
+ 
  void createlist(char *buff)
  {
  	NODE *temp;
@@ -68,7 +111,6 @@ typedef struct node
 		}
 		scanPtr->next=temp;
 	}
-	//printf("%s => ",temp->data);
  }
  void DisplayList()
  {
@@ -92,3 +134,25 @@ typedef struct node
 	 }
 	 
  }
+ 
+/* function to search a node in the linked list*/
+
+ void SearchListReplace()
+ {
+ 	char search_word[30];
+ 	char replace_word[30];
+ 	printf("entr the element to be searched :-\n");
+ 	scanf("%s",search_word);
+ 	printf("entr the element to be searched :-\n");
+ 	scanf("%s",replace_word);
+ 	
+ 	scanPtr=head;				//assigning starting node address to scanPtr
+ 	
+ 	/* seraching for an given element in the linked list till the last node in the list*/	
+ 	while(scanPtr!=NULL)
+ 	{
+ 		if(strcmp(scanPtr->data,search_word)==0)
+ 		    strcpy(scanPtr->data,replace_word);
+		scanPtr=scanPtr->next;
+	}
+}
