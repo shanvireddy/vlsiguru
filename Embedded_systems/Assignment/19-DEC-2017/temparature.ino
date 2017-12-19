b4ceadb7aaf7e53e6fe069c1cce0d0c83f5d6a70
vanitha <31364598+shanvireddy@users.noreply.github.com>
2017-12-19 12:20:17 +0530

    Add files via upload

---
+++ Embedded_systems/Assignment/19-DEC-2017/temparature.ino
@@ -0,0 +1,48 @@
+// program to interface DHT11 temerature sensor with arduino
+
+
+#include <dht.h>
+#define dht_apin A0 // Analog Pin sensor is connected to A0
+
+int ledpin=13;      
+dht DHT;
+ 
+void setup()
+{
+  pinMode(ledpin,OUTPUT);
+  Serial.begin(9600);
+  delay(500);//Delay to let system boot
+  Serial.println("DHT11 Humidity & temperature Sensor\n\n");
+  delay(500);//Wait before accessing Sensor
+ 
+}//end "setup()"
+ 
+void loop()
+{
+  //Start of Program 
+ 
+    DHT.read11(dht_apin);
+    
+    Serial.print("Current humidity = ");
+    Serial.print(DHT.humidity);
+    Serial.print("  %   ");
+    Serial.print("temperature = ");
+    Serial.print(DHT.temperature); 
+    Serial.println("  C  ");   
+    delay(500);//Wait 5 seconds before accessing sensor again.
+    if(DHT.temperature>=40)
+    {
+      digitalWrite(ledpin,HIGH);   // set the led when temperature reading .=40C
+      delay(5000);
+    }
+    else
+    {
+      digitalWrite(ledpin,LOW);  
+    }
+  
+ 
+}// end loop() 
+
+
+
+
